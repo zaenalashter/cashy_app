79ddcae6d974a3b2c5c4f045cd508195ec9f1b06
zaenalashter <santrialit77@gmail.com>
2022-05-10 21:54:06 +0700

    padding Widget

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,10 @@ class MyApp extends StatelessWidget {
                   image: AssetImage('assets/images/payments.png'),
                   height: 300,
                 ),
-                Text("Rich Together", style: mainHeader),
+                Padding(
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: Text("Rich Together", style: mainHeader),
+                ),
                 Text(
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
                   style: subHeader,
